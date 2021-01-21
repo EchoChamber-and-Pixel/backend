82f8e821a459e9d3ec434977d9fb00d30fe21b39
Brad Whitesell <whitesellbt@gmail.com>
2021-01-21 00:16:59 -0800

    Initial commit

---
+++ backend/backend/Controllers/RecordsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecordsController : ControllerBase
+    {
+        private readonly ILogger<RecordsController> _logger;
+
+        public RecordsController(ILogger<RecordsController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Gets all records.
+        /// </summary>
+        /// <returns>List of records.</returns>
+        [HttpGet]
+        public List<string> Get()
+        {
+            return new List<string>() { "EchoFrost - All WRs" };
+        }
+    }
+}
